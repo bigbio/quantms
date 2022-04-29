dfd691921a364fa24181fa279489df5ed34e60ca
Yasset Perez-Riverol <ypriverol@gmail.com>
2022-04-29 20:02:28 +0100

    first iteration of utils msstats_utils.R

--- bin/msstats_plfq.R
+++ bin/msstats_plfq.R
@@ -5,7 +5,7 @@ require(tibble)
 require(data.table)
 require(here)
 
-source(here::here('msstats_utils.R'))
+source(here::here('bin/msstats_utils.R'))
 
 usage <- "Rscript msstats_plfq.R input.csv [list of contrasts or 'pairwise'] [default control condition or ''] ..."
 char_to_boolean <- c("true"=TRUE, "false"=FALSE)
